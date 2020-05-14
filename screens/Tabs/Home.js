0af9c74a56ffc99ea6981e6ebc2cd5dc02fdbd3f
DS Hwang <hds2g22@gmail.com>
2020-05-14 12:50:18 +0900

    #15.0 Post Component Header

--- screens/Tabs/Home.js
+++ screens/Tabs/Home.js
@@ -4,8 +4,7 @@ import styled from "styled-components";
 import Loader from "../../components/Loader";
 import { gql } from "apollo-boost";
 import { useQuery } from "react-apollo-hooks";
-
-const Text = styled.Text``;
+import Post from "../../components/Post";
 
 const FEED_QUERY = gql`
   {
@@ -64,7 +63,13 @@ export default () => {
         <RefreshControl refreshing={refreshing} onRefresh={refresh} />
       }
     >
-      {loading ? <Loader /> : <Text>Hello</Text>}
+      {loading ? (
+        <Loader />
+      ) : (
+        data &&
+        data.seeFeed &&
+        data.seeFeed.map((post) => <Post key={post.id} {...post} />)
+      )}
     </ScrollView>
   );
 };

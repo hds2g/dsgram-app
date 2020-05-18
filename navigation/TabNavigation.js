72bf3f1cff12e5f28bad65ba1f306864345c2689
DS Hwang <hds2g22@gmail.com>
2020-05-18 11:49:36 +0900

    #16.0/1 Search as HeaderTitle

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -39,9 +39,7 @@ export default createBottomTabNavigator(
       },
     },
     Search: {
-      screen: stackFactory(Search, {
-        title: "Search",
-      }),
+      screen: stackFactory(Search),
       navigationOptions: {
         tabBarIcon: ({ focused }) => (
           <NavIcon
@@ -101,6 +99,7 @@ export default createBottomTabNavigator(
     },
   },
   {
+    initialRouteName: "Search",
     tabBarOptions: {
       showLabel: false,
       style: {

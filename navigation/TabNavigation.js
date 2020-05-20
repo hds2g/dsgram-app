46f4f46712c527627af9d9f109932de2557e17f4
DS Hwang <hds2g22@gmail.com>
2020-05-20 14:41:25 +0900

    #17.0 Profile Screen

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -120,7 +120,7 @@ export default createBottomTabNavigator(
     },
   },
   {
-    initialRouteName: "Search",
+    initialRouteName: "Profile",
     tabBarOptions: {
       showLabel: false,
       style: {
